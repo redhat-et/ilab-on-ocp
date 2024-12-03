d4e91ee905f8a221b3206c2ac758097b8ba5c190
Michael Clifford <mcliffor@redhat.com>
2024-12-03 14:14:27 -0500

    update rhelai 1.2 to 1.3
    Signed-off-by: Michael Clifford <mcliffor@redhat.com>

--- utils/consts.py
+++ utils/consts.py
@@ -1,4 +1,4 @@
 PYTHON_IMAGE = "quay.io/modh/odh-generic-data-science-notebook:v3-2024b-20241111"
 TOOLBOX_IMAGE = "registry.access.redhat.com/ubi9/toolbox"
 OC_IMAGE = "registry.redhat.io/openshift4/ose-cli"
-RHELAI_IMAGE = "quay.io/redhat-et/ilab:1.2"
+RHELAI_IMAGE = "quay.io/redhat-et/ilab:1.3"
