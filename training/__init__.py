16d0270e5d66e90e26a48153db035b4ae4448664
Shreyanand <shanand@redhat.com>
2024-12-03 21:47:07 -0500

    Update launcher component with correct params
    Signed-off-by: Shreyanand <shanand@redhat.com>

--- training/__init__.py
+++ training/__init__.py
@@ -2,13 +2,13 @@ from . import faked
 from .components import (
     data_processing_op,
     knowledge_processed_data_to_artifact_op,
-    pytorchjob_manifest_op,
+    pytorch_job_launcher_op,
     skills_processed_data_to_artifact_op,
 )
 
 __all__ = [
     "data_processing_op",
-    "pytorchjob_manifest_op",
+    "pytorch_job_launcher_op",
     "skills_processed_data_to_artifact_op",
     "knowledge_processed_data_to_artifact_op",
     "faked",
